ed5a41c2a914217ddf3ab5e0aba3cfc3ff2d5deb
anyway <346454236@qq.com>
2024-09-15 09:11:38 +0800

    Create disable_battleground.sql

---
+++ data/sql/db-world/updates/disable_battleground.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 7, 0, '0', '', 'disable some battlegrounds');
+REPLACE INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 9, 0, '0', '', 'disable some battlegrounds');
+REPLACE INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 30, 0, '0', '', 'disable some battlegrounds');
+REPLACE INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 32, 0, '0', '', 'disable some battlegrounds');
